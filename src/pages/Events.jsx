ae2fcab6c5babd6aee220b841c0d8f4315913917
Alibayejja <alibayejja604@gmail.com>
2025-12-23 16:33:18 +0000

    first commit

---
+++ src/pages/Events.jsx
@@ -0,0 +1,10 @@
+function Events() {
+    return (
+        <div>
+            <h2>Page Events</h2>
+            <p>هنا غادي نعرضو events فـ cards من بعد.</p>
+        </div>
+    );
+}
+
+export default Events;
